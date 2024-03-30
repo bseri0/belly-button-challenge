caf4ba6d649314a8577f2657315f274f5dbf4bb3
Bryan S <146388490+bseri0@users.noreply.github.com>
2024-03-29 22:35:11 -0400

    new file:   StarterCode/static/js/_app.js 	modified:   StarterCode/static/js/app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,73 +1,111 @@
-// The URL with data
-const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
+var dataUrl = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
-// Fetch the JSON data and initialize dropdown and first set of plots
-function init() {
-    d3.json(url).then((data) => {
-        const dropdownMenu = d3.select("#selDataset");
-        data.names.forEach((name) => dropdownMenu.append("option").text(name).property("value", name));
-        
-        // Initialize plots with the first name
-        updateAll(data, data.names[0]);
+function initialize() {
+    d3.json(dataUrl).then(function(dataResponse) {
+        console.log("Look at this data:", dataResponse);
+
+        var dropDown = d3.select("#selDataset");
+
+        for(var i = 0; i < dataResponse.names.length; i++) {
+            dropDown.append("option").text(dataResponse.names[i]).property("value", dataResponse.names[i]);
+        }
+
+        var firstItem = dataResponse.names[0];
+        makeDemoInfo(firstItem);
+        makeBarChart(firstItem);
+        makeBubbleChart(firstItem);
+        makeGaugeChart(firstItem);
     });
 }
 
-// Update demographic info, bar chart, bubble chart, and gauge chart
-function updateAll(data, selectedValue) {
-    updateDemo(data.metadata.filter(meta => meta.id == selectedValue)[0]);
-    updateCharts('bar', data.samples.filter(sample => sample.id === selectedValue)[0]);
-    updateCharts('bubble', data.samples.filter(sample => sample.id === selectedValue)[0], true);
-    updateGauge(data.metadata.filter(meta => meta.id == selectedValue)[0].wfreq);
-    updateGauge(data.metadata.filter(meta => meta.id == selectedValue)[1].wfreq);
-    
+function makeDemoInfo(id) {
+    d3.json(dataUrl).then(function(data) {
+        var filteredMeta = data.metadata.filter(function(item) {
+            return item.id == id;
+        })[0];
 
-}
+        var infoPanel = d3.select("#sample-metadata");
+        infoPanel.html("");
 
-function updateDemo(metadata) {
-    const demographicInfo = d3.select("#sample-metadata").html("");
-    Object.entries(metadata).forEach(([key, value]) => demographicInfo.append("h5").text(`${key}: ${value}`));
-}
-
-function updateCharts(chartId, sampleData, isBubble = false) {
-    const yVals = sampleData.otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse();
-    const trace = {
-        x: isBubble ? sampleData.otu_ids : sampleData.sample_values.slice(0, 10).reverse(),
-        y: isBubble ? sampleData.sample_values : yVals,
-        text: isBubble ? sampleData.otu_labels : sampleData.otu_labels.slice(0, 10).reverse(),
-        mode: isBubble ? 'markers' : '',
-        marker: isBubble ? {size: sampleData.sample_values, color: sampleData.otu_ids, colorscale: "Portland"} : {color: "rgb(240,128,128)"},
-        type: isBubble ? 'scatter' : 'bar',
-        orientation: isBubble ? '' : 'h'
-    };
-    const layout = isBubble ? {xaxis: {title: "OTU ID"}} : {};
-    Plotly.newPlot(chartId, [trace], layout);
-}
-
-function updateGauge(wfreq) {
-    const data = [{
-        domain: {x: [0, 1], y: [0, 1]},
-        value: wfreq,
-        title: {text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week", font: {size: 24}},
-        type: "indicator",
-        mode: "gauge+number",
-        gauge: {
-            axis: {range: [null, 9]},
-            steps: [
-                // Define your gauge chart color steps here
-            ],
-            threshold: {
-                line: {color: "red", width: 4},
-                thickness: 0.75,
-                value: wfreq
-            }
+        for (var key in filteredMeta) {
+            infoPanel.append("p").text(key + ": " + filteredMeta[key]);
         }
-    }];
-    Plotly.newPlot('gauge', data);
+    });
 }
 
-// Handle change in dropdown selection
-function optionChanged(newSample) {
-    d3.json(url).then((data) => updateAll(data, newSample));
+function makeBarChart(id) {
+    d3.json(dataUrl).then(function(data) {
+        var samples = data.samples.filter(function(item) {
+            return item.id === id;
+        })[0];
+
+        var trace = {
+            x: samples.sample_values.slice(0, 10).reverse(),
+            y: samples.otu_ids.slice(0, 10).map(function(otuID) { return "OTU " + otuID; }).reverse(),
+            text: samples.otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h"
+        };
+
+        var layout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: { t: 30, l: 150 }
+        };
+
+        Plotly.newPlot("bar", [trace], layout);
+    });
 }
 
-init();
+function makeBubbleChart(id) {
+    d3.json(dataUrl).then(function(data) {
+        var samples = data.samples.filter(function(item) {
+            return item.id === id;
+        })[0];
+
+        var trace = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            }
+        };
+
+        var layout = {
+            title: "Bacteria Cultures Per Sample",
+            showlegend: false,
+            height: 600,
+            width: 1200
+        };
+
+        Plotly.newPlot("bubble", [trace], layout);
+    });
+}
+
+function makeGaugeChart(id) {
+    d3.json(dataUrl).then(function(data) {
+        var meta = data.metadata.filter(function(item) {
+            return item.id == id;
+        })[0];
+
+        var data = [{
+            domain: { x: [0, 1], y: [0, 1] },
+            value: meta.wfreq,
+            title: { text: "Washes Per Week" },
+            type: "indicator",
+            mode: "gauge+number"
+        }];
+
+        Plotly.newPlot("gauge", data);
+    });
+}
+
+function optionChanged(newID) {
+    makeDemoInfo(newID);
+    makeBarChart(newID);
+    makeBubbleChart(newID);
+    makeGaugeChart(newID);
+}
+
+initialize();
